7fa76c3cccabf8f2d65a8551e49fe3b413a56bf9
raymondjaramillo <rjaramillo155@cnm.edu>
2020-02-14 10:22:49 -0700

    Added Post/Delete to SourceController.java and extra "location" header in QuoteController.java.

--- src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
@@ -4,11 +4,13 @@ import edu.cnm.deepdive.qod.model.entity.Quote;
 import edu.cnm.deepdive.qod.model.entity.Source;
 import edu.cnm.deepdive.qod.service.QuoteRepository;
 import edu.cnm.deepdive.qod.service.SourceRepository;
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,27 +20,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import sun.security.util.ManifestEntryVerifier;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("quotes")
-
 public class QuoteController {
 
   private final QuoteRepository quoteRepository;
   private final SourceRepository sourceRepository;
 
   @Autowired
-  public QuoteController(QuoteRepository quoteRepository,
-      SourceRepository sourceRepository) {
+  public QuoteController(QuoteRepository quoteRepository, SourceRepository sourceRepository) {
     this.quoteRepository = quoteRepository;
     this.sourceRepository = sourceRepository;
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
-  public Quote post(@RequestBody Quote quote) {
-    return quoteRepository.save(quote);
+  public ResponseEntity<Quote> post(@RequestBody Quote quote) {
+    quoteRepository.save(quote);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .build(quote.getId());
+    return ResponseEntity.created(location).body(quote);
   }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -52,7 +56,7 @@ public class QuoteController {
   }
 
   @PutMapping(value = "{id}",
-  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote put(@PathVariable UUID id, @RequestBody Quote modifiedQuote) {
     Quote quote = get(id);
     quote.setText(modifiedQuote.getText());
@@ -68,30 +72,20 @@ public class QuoteController {
     return quote.getText();
   }
 
-
   @DeleteMapping(value = "{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID id) {
-    // Code below throws NoSuchElementException if id is not in database
+    // Code below throws NoSuchElementException if id is not in database.
 //    Quote quote = get(id);
 //    quoteRepository.delete(quote);
     quoteRepository.findById(id).ifPresent(quoteRepository::delete);
   }
 
   @PutMapping(value = "{quoteId}/sources/{sourceId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Quote attach(@PathVariable UUID quoteId,@PathVariable UUID sourceId) {
+  public Quote attach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
     Quote quote = get(quoteId);
     Source source = sourceRepository.findById(sourceId).get();
-    List<Source> sources = quote.getSources();
-    boolean matchFound = false;
-    for (Source s : sources) {
-      if (s.getId().equals(source.getId())) {
-        matchFound = true;
-        break;
-      }
-    }
-    if (!matchFound) {
-      sources.add(source);
+    if (quote.getSources().add(source)) {
       quoteRepository.save(quote);
     }
     return quote;
@@ -99,21 +93,12 @@ public class QuoteController {
 
   @DeleteMapping(value = "{quoteId}/sources/{sourceId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void detach(@PathVariable UUID quoteId,@PathVariable UUID sourceId) {
+  public void detach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
     Quote quote = get(quoteId);
     Source source = sourceRepository.findById(sourceId).get();
-    List<Source> sources = quote.getSources();
-    boolean matchFound = false;
-    for (Source s : sources) {
-      if (s.getId().equals(source.getId())) {
-        matchFound = true;
-        break;
-      }
-    }
-    if (matchFound) {
-      sources.remove(source);
+    if (quote.getSources().remove(source)) {
       quoteRepository.save(quote);
     }
   }
-}
 
+}
--- src/main/java/edu/cnm/deepdive/qod/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/qod/model/entity/Quote.java
@@ -1,8 +1,10 @@
 package edu.cnm.deepdive.qod.model.entity;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -21,9 +23,7 @@ import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 
 @Entity
 @Table(
@@ -33,56 +33,44 @@ import org.springframework.lang.Nullable;
 )
 public class Quote {
 
-
   @NonNull
-  @Id
-  @GeneratedValue(generator = "uuid2")
+  @Id@GeneratedValue(generator = "uuid2")
   @GenericGenerator(name = "uuid2", strategy = "uuid2")
   @Column(name = "quote_id", columnDefinition = "CHAR(16) FOR BIT DATA",
       nullable = false, updatable = false)
   private UUID id;
 
   @NonNull
- @CreationTimestamp
- @Temporal(TemporalType.TIMESTAMP)
- @Column(nullable = false, updatable = false)
- private Date created;
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
 
- @NonNull
- @UpdateTimestamp
- @Temporal(TemporalType.TIMESTAMP)
- @Column(nullable = false)
- private Date updated;
+  @NonNull
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
 
- @NonNull
- @Column(length = 4096, nullable = false, unique = true)
+  @NonNull
+  @Column(length = 4096, nullable = false, unique = true)
   private String text;
 
- @NonNull
- @ManyToMany(
-     fetch = FetchType.LAZY,
- cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
- )
-  @JoinTable(
-      joinColumns = {
-          @JoinColumn(name = "quote_id")
-      },
-      inverseJoinColumns = {
-          @JoinColumn(name = "source_id")
-      }
+  @NonNull
+  @ManyToMany(
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
   )
- @OrderBy("name ASC")
- private List<Source> sources = new LinkedList<>();
-
+  @JoinTable(
+      joinColumns = @JoinColumn(name = "quote_id"),
+      inverseJoinColumns = @JoinColumn(name = "source_id")
+  )
+  @OrderBy("name ASC")
+  private Set<Source> sources = new LinkedHashSet<>();
 
   @NonNull
   public UUID getId() {
     return id;
-  }
-
-  @NonNull
-  public String getText() {
-    return text;
   }
 
   @NonNull
@@ -95,12 +83,18 @@ public class Quote {
     return updated;
   }
 
+  @NonNull
+  public String getText() {
+    return text;
+  }
+
   public void setText(@NonNull String text) {
     this.text = text;
   }
 
   @NonNull
-  public List<Source> getSources() {
+  public Set<Source> getSources() {
     return sources;
   }
+
 }
